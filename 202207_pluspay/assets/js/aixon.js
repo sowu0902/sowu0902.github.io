978e64abb5ef14ec86b15379618af74fffb5a760
SOWU <sophiawu@cw.com.tw>
2022-07-11 00:15:42 +0800

    切版未完-品牌活動未完成

---
+++ 202207_pluspay/assets/js/aixon.js
@@ -0,0 +1,17 @@
+AIQUA.init({
+  appId: '9cca20217baec9fc0f04',
+  timeout: 5000,
+  });
+  
+  !function (q, g, r, a, p, h, js) {
+  if (q.qg) return;
+  js = q.qg = function () {
+      js.callmethod ? js.callmethod.call(js, arguments) : js.queue.push(arguments);
+  };
+  js.queue = [];
+  p = g.createElement(r);
+  p.async = !0;
+  p.src = a;
+  h = g.getElementsByTagName(r)[0];
+  h.parentNode.insertBefore(p, h);
+  }(window, document, 'script', 'https://cdn.qgr.ph/qgraph.9cca20217baec9fc0f04.js');
