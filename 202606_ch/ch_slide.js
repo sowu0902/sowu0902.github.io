c7da28b9ea1c71a1717afdc199544d7ddac7f064
sophia <sowu0902@gmail.com>
2026-07-29 16:52:17 +0800

    把css和js存成獨立檔案

---
+++ 202606_ch/ch_slide.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.ch_slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 900,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000
+    });
+  });
